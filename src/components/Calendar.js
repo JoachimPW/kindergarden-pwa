e4b183c6f8923c5d46077b01af51b514eed01f0f
KennethJegindfeldt <jegindfeldt@gmail.com>
2019-05-16 12:44:19 +0200

    gey

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -11,21 +11,7 @@ export default class Calendar extends Component {
         this.state = {
             requiredItem: 0,
             brochure: [
-                {
-                    dato: "Mandag d. 3. juni",
-                    afleveret: "08:15",
-                    afhentet: "16:00"
-                },
-                {
-                    dato: "Tirsdag d. 4. juni",
-                    afleveret: "08:15",
-                    afhentet: "16:00"
-                },
-                 {
-                    dato: "Onsdag d. 5. juni",
-                    afleveret: "08:15",
-                    afhentet: "16:00"
-                }
+            
             ]
         }
     }
@@ -57,9 +43,18 @@ export default class Calendar extends Component {
             <article>
               <div class="col-lg-12">
                 <div class="card">
-                  <h1>{elm.date}</h1>
-                  <h4>{elm.in}</h4>
-                  <h4>{elm.out}</h4>
+               
+                <h3>{elm.date}</h3>
+                    <fieldset>
+                        <label>Aflever</label>
+                        <input type="text" value={elm.in} id="input_from" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label>Hent</label>
+                        <input type="text" value={elm.out} id="input_to" />
+                    </fieldset>
+
                 </div>
                 <hr></hr>
               </div>
@@ -67,56 +62,24 @@ export default class Calendar extends Component {
           </React.Fragment>)
     
         });
-        if (this.state.brochure.length > 0) {
-            const brochure = this.state.brochure.map((item, index) => {
-                return (
-                    <React.Fragment>
-                    {dayList}
-                    <article>
-                        <div key={index} class="col-lg-12">
-                            <div class="card">
-                                <h1 style={{textAlign: "center"}}>{item.dato}</h1>
-                                <h2>Aflevere:  {item.afleveret}</h2>
-                                <h2>Afhent: {item.afhentet}</h2>
-                                <div class="col-lg-4">
-                                    <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal"
-                                        onClick={() => this.replaceModalItem(index)}>edit</button> {" "}                                    
-                                </div>
-                            </div>
-                            <hr></hr>
-                        </div>
-                    </article>
-                    </React.Fragment>
-                )
-            });
+     
 
-            const requiredItem = this.state.requiredItem;
-            let modalData = this.state.brochure[requiredItem];
-            return (
+        return (            
+            <React.Fragment>
                 <div class="wrapper">
                     <div id="content">
-                    <Header></Header>
+
+                        <Header></Header>        
                         <div class="container">
                             <div class="col-lg-12">
-                                <h1>Calendar</h1>
+                                <h1>Kalender</h1>
                             </div>
-                            {brochure}
-                            <React.Fragment>
-                                <div className="overlay"></div>
-                                <div>
-                                    <Modal
-                                        dato={modalData.dato}
-                                        afleveret={modalData.afleveret}
-                                        afhentet={modalData.afhentet}
-                                        saveModalDetails={this.saveModalDetails}
-                                    />
-                                </div>
-                            </React.Fragment>
+                            {dayList}
                         </div>
                     </div>
-                </div>
-            )
-        }
-        return <h1>INGEN DATA</h1>
+                </div>                      
+             </React.Fragment>
+
+                );
     }
 }
